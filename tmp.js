8605166e0c84d4b780494646760e62056def76be
zsb <2798528624@qq.com>
2022-08-21 13:03:39 +0800

    2

--- tmp.js
+++ tmp.js
@@ -1,18 +1,18 @@
 // ==UserScript==
 // @name         helloworld
 // @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  try to take over the world!
-// @author       You
+// @version      0.2
+// @description  start
+// @author       zsv
 // @match        *://*/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=bilibili.com
-// @grant        none 
 // @license      MIT
+// @grant        none 
 // ==/UserScript==
 
 (function() {
     'use strict';
 
     // Your code here...
-    console.log('Hello World!');
+    console.log('Hello World2!');
 })();
