fd509d71e9e7764e422dfd45ce0a36abea8f2996
zsb <2798528624@qq.com>
2022-08-21 12:52:34 +0800

    1

---
+++ tmp.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         helloworld
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        *://*/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=bilibili.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    console.log('Hello World!');
+})();
